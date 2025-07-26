5044dbe300de397afe2de3850ebedffcff230234
Taranchuk <nfhfyxer@gmail.com>
2025-07-26 12:29:59 +0300

    fix

--- 1.6/Source/Utils.cs
+++ 1.6/Source/Utils.cs
@@ -36,7 +36,8 @@ namespace Worldbuilder
         public static List<T> GetSurfaceWorldObjects<T>() where T : WorldObject
         {
             var planetLayer = Find.WorldGrid.FirstLayerOfDef(PlanetLayerDefOf.Surface);
-            return Find.World.worldObjects.worldObjects.OfType<T>().Where(x => x.Tile.Layer == planetLayer && x.Faction?.def?.isPlayer is false).ToList();
+            return Find.World.worldObjects.worldObjects.OfType<T>().Where(x => x.Tile.Layer == planetLayer && (x.Faction?.def?.isPlayer is false || x.def == DefsOf
+            .WB_MapMarker)).ToList();
         }
 
         public static void LogMessage(this Thing thing, string message)
